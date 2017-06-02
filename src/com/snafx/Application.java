1e9424270fd0714a3cffbf48cf2827a5625ed1c0
unknown <k.ozegowski@gmail.com>
2017-06-02 17:52:36 +0200

    Initial commit

---
+++ src/com/snafx/Application.java
@@ -0,0 +1,10 @@
+package com.snafx;
+
+/**
+ * Created by RENT on 2017-06-02.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
