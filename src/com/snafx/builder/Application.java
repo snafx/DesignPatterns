bfac40a17d166a3fe5961136003a569082819bc4
unknown <k.ozegowski@gmail.com>
2017-06-05 18:44:49 +0200

    Added lombok to builder example

--- src/com/snafx/builder/Application.java
+++ src/com/snafx/builder/Application.java
@@ -1,15 +1,12 @@
 package com.snafx.builder;
 
-/**
- * Created by RENT on 2017-06-05.
- */
 public class Application {
     public static void main(String[] args) {
 
         User user = User.builder()
                 .firstName("Marian")
                 .lastName("Kupa")
-                .phoneNumber("666-666-666")
+                .phoneNumber("666-777-8888")
                 .age(66)
                 .build();
 
--- src/com/snafx/builder/User.java
+++ src/com/snafx/builder/User.java
@@ -1,91 +1,22 @@
 package com.snafx.builder;
 
-/**
- * Created by RENT on 2017-06-05.
- */
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Builder
+@Data
+
 public class User {
+
+//    mozna dowolnie modyfikowac gettery, setter, buildera, toStringa etc
+//    @Getter(value = AccessLevel.PRIVATE)
+
     private String firstName;
     private String lastName;
     private String phoneNumber;
     private int age;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", age=" + age +
-                '}';
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public static UserBuilder builder() {
-        return new UserBuilder();
-    }
-
-    public static class UserBuilder {
-        private String firstName;
-        private String lastName;
-        private String phoneNumber;
-        private int age;
-
-        public UserBuilder firstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-        public UserBuilder lastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-        public UserBuilder phoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
-            return this;
-        }
-        public UserBuilder age(int age) {
-            this.age = age;
-            return this;
-        }
-
-        public User build() {
-            User user = new User();
-            user.setFirstName(firstName);
-            user.setLastName(lastName);
-            user.setPhoneNumber(phoneNumber);
-            user.setAge(age);
-
-            return user;
-        }
-    }
 }
